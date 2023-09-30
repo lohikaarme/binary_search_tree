9c177892d18d4cb75279337722205873e7f85ace
lohikaarme <stewart.watt@gmail.com>
2023-09-29 22:58:10 -0700

    Added #pretty_print from TOP documentation

--- bst.rb
+++ bst.rb
@@ -1,9 +1,9 @@
 class Node
   include Comparable
-  attr_accessor :list, :left, :right
+  attr_accessor :data, :left, :right
 
-  def initialize (list = nil, left = nil, right = nil)
-    @list = list
+  def initialize (data = nil, left = nil, right = nil)
+    @data = data
     @left = left
     @right = right
   end
@@ -14,12 +14,13 @@ class Tree
 attr_accessor :root
 
   def initialize(values)
-    @values = values
+    @values = values.sort
     @root = nil
   end
 
   def build_tree(values)
     return nil if values.empty?
+    values = values.sort
 
     count = values.size
     mid = count / 2
@@ -38,7 +39,7 @@ attr_accessor :root
         mid = (left + right) / 2
         child = Node.new(values[mid])
 
-        if values[mid] < parent.list
+        if values[mid] < parent.data
           parent.left = child
         else
           parent.right = child
@@ -55,17 +56,37 @@ attr_accessor :root
   def printBST(root)
     return if root.nil?
 
-    print "#{root.list} "
+    print "#{root.data} "
     printBST(root.left)
     printBST(root.right)
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def find(value)
+
+  end
+
+  def insert(value)
+
+  end
+
+  def delete(value)
+
   end
 
 end
 
 
 
-test = [1,2,,3,4,8]
+test = [1,2,7,3,4,8]
 tree = Tree.new(test)
 tree3 = tree.build_tree(test)
 tree.printBST(tree3)
-
+print "\n"
+tree.pretty_print(tree3)
+1+1
